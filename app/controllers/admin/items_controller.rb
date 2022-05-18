f2d049f82df4a1991907498ea162ff3da4b8afb7
EC2 Default User <ec2-user@ip-172-31-30-62.ec2.internal>
2022-05-18 13:53:24 +0000

    エラー解消のお付き合いお願いします

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,30 +11,26 @@ class Admin::ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
 
   def create
     @item = Item.new(item_params)
-    if @item.save
-      flash[:notice] = "商品を登録しました"
-      redirect_to admin_item_path(@item.id)
-    else
-      render "new"
-    end
+    @item.save
+     flash[:notice] = "商品を登録しました"
+     redirect_to admin_item_path
   end
 
   def update
     @item = Item.find(params[:id])
-    if @item.update(item_params)
-      flash[:notice] = "商品情報を更新しました"
-      redirect_to admin_item_path(@item.id)
-    else
-      render "edit"
-    end
+    @item.update(item_params)
+     flash[:notice] = "商品情報を更新しました"
+     redirect_to admin_item_path
   end
 
   private
