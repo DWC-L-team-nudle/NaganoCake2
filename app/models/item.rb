12a7b0d14b80d0e1be52b5a705040f490183b111
oakay455 <ayako110.89@gmail.com>
2022-05-18 03:56:16 +0000

    [Add]会員側items/index

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  has_one_attached :item_image
+  
+  belongs_to :genre
+
+  def with_tax_price
+        (price * 1.1).floor
+  end
+
+
+  def get_item_image
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image
+  end
 end
