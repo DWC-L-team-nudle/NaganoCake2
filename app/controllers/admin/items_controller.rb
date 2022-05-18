5b26165077cc69f9298a99a4f41ab592339aab8f
EC2 Default User <ec2-user@ip-172-31-30-62.ec2.internal>
2022-05-18 12:11:01 +0000

    アイテム一覧画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,46 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "商品を登録しました"
+      redirect_to admin_item_path(@item.id)
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "商品情報を更新しました"
+      redirect_to admin_item_path(@item.id)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
+    end
+
 end
