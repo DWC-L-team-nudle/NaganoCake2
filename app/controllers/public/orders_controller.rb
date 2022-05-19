e3c8d67a67dfe779ebf336dba0c8bb4a6ea0a1b5
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-05-19 08:36:11 +0000

    [ADD]注文履歴詳細を書き忘れてました

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,7 +1,10 @@
 class Public::OrdersController < ApplicationController
   def index
     customer = Customer.find_by(params[:customer_id])
-    @orders = customer.orders
+    @orders = customer.orders.order(created_at: :desc)
   end
 
+  def show
+    @orders = Order.find(params[:id])
+  end
 end
