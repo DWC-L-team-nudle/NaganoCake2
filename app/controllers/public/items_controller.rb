9788979085f2b509cdb8f2130d441f6c8aa56701
oakay455 <ayako110.89@gmail.com>
2022-05-21 03:37:48 +0000

    [Add]ジャンル表示

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]) #config/initializers/kaminari_config.rbに表示件数の指定必要(デフォルトは25件)
-    # @genres = Genre.all
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
-    #@genre = Genre.all
+    @genres = Genre.all
   end
 
 
