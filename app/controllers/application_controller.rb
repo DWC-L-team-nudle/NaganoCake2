e319def5c3d98a44f73e162744c6d9bd43c6265c
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-05-24 04:53:02 +0000

    [Fix]Error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
 
+private
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+  end
+
+  def reject_customer
+    @customer = Customer.find_by(name: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to new_customer_registration
+      end
+    end
+  end
 end
