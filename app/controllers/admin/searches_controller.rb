ba65b13f0363922cb5cc8ce73b18442fb53c5626
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-05-24 13:07:01 +0000

    [Fix]Layout

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,7 +1,10 @@
 class Admin::SearchesController < ApplicationController
   def search
+    @word = params[:word]
     if params[:word].present?
       @items = Item.where('name LIKE ?', "%#{params[:word]}%").page(params[:page]).per(10)
+    else
+      @items = Item.all.page(params[:page]).per(10)
     end
   end
 end
