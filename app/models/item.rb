87a58327601fb3aea669d86d6ba607b7f7842914
minoriakita <minori.akita@gmail.com>
2022-05-25 02:13:43 +0000

    fix レイアウト、商品バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,10 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
 
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+
   def with_tax_price
         (price * 1.1).floor
   end
